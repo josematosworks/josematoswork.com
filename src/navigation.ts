0d07833152fb9efb7b865b223bd561cb925893bf
José Matos <developer@josematoswork.com>
2024-10-23 17:13:14 +0200

    style(format): apply format using prettier

--- src/navigation.ts
+++ src/navigation.ts
@@ -1,27 +1,49 @@
-import { getPermalink, getAsset } from './utils/permalinks';
+import { getPermalink, getAsset } from "./utils/permalinks";
 
 export const headerData = {
   links: [
-    { text: 'Home', href: '/' },
-    { text: 'About', href: '#about' },
-    { text: 'Resume', href: '#resume' },
-    { text: 'Portfolio', href: '#portfolio' },
-    { text: 'Github', href: 'https://github.com/josematosworks' },
+    { text: "Home", href: "/" },
+    { text: "About", href: "#about" },
+    { text: "Resume", href: "#resume" },
+    { text: "Portfolio", href: "#portfolio" },
+    { text: "Github", href: "https://github.com/josematosworks" },
   ],
-  actions: [{ text: 'Download CV', href: '/pdfs/Jose_Matos_Senior_Frontend_Engineer_Resume.pdf', target: '_blank' }],
+  actions: [
+    {
+      text: "Download CV",
+      href: "/pdfs/Jose_Matos_Senior_Frontend_Engineer_Resume.pdf",
+      target: "_blank",
+    },
+  ],
 };
 
 export const footerData = {
   secondaryLinks: [
-    { text: 'Terms', href: getPermalink('/terms') },
-    { text: 'Privacy Policy', href: getPermalink('/privacy') },
+    { text: "Terms", href: getPermalink("/terms") },
+    { text: "Privacy Policy", href: getPermalink("/privacy") },
   ],
   socialLinks: [
-    { ariaLabel: 'X', icon: 'tabler:brand-x', href: 'https://x.com/josematosworks' },
-    { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: 'https://www.instagram.com/josematosworks' },
-    { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: 'https://www.facebook.com/josematosworks' },
-    { ariaLabel: 'RSS', icon: 'tabler:rss', href: getAsset('/rss.xml') },
-    { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/josematosworks' },
+    {
+      ariaLabel: "X",
+      icon: "tabler:brand-x",
+      href: "https://x.com/josematosworks",
+    },
+    {
+      ariaLabel: "Instagram",
+      icon: "tabler:brand-instagram",
+      href: "https://www.instagram.com/josematosworks",
+    },
+    {
+      ariaLabel: "Facebook",
+      icon: "tabler:brand-facebook",
+      href: "https://www.facebook.com/josematosworks",
+    },
+    { ariaLabel: "RSS", icon: "tabler:rss", href: getAsset("/rss.xml") },
+    {
+      ariaLabel: "Github",
+      icon: "tabler:brand-github",
+      href: "https://github.com/josematosworks",
+    },
   ],
   footNote: `
     Made by José Matos. Powered by Astro. All rights reserved.
